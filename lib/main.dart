4301bcfe7a61f8a08b15d048449e22fd6f600cd7
Animesh Murmu <am2646374@gmail.com>
2021-12-28 16:26:53 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -22,49 +22,7 @@ class App extends StatelessWidget {
         backgroundColor: Colors.green[900],
       ),
       
-      body: Row(
-        // mainAxisAlignment: MainAxisAlignment.end,
-        // mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        
-        children:  [
-          const Text("hello"),
-          Image.asset(
-            "assets/pexels-photo-1535162.jpeg",
-            scale: 10,
-            ),
-          const Icon(
-            Icons.access_alarms,
-            color: Colors.amber,
-            ),
-          const IconButton(
-            onPressed: null, 
-            icon: Icon(
-              Icons.accessibility,
-              color: Colors.blue,
-              ),
-            iconSize: 50,
-            ),
-          TextButton.icon(
-            onPressed: null,
-              icon: const Icon(Icons.account_balance),
-              label: const Text(
-                "App"
-                ),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>((Colors.yellow[300])!),
-                ),
-              )
-
-        ],
-      ),
+      body: Column(),
 
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
