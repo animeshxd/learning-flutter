c40e92bb572432a54b63ebe96b9100cccaac6afd
Animesh Murmu <am2646374@gmail.com>
2021-12-28 14:38:54 +0530

    CONTAINER INIT

--- lib/main.dart
+++ lib/main.dart
@@ -21,66 +21,7 @@ class App extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[900],
       ),
-      body:  const Center(
-        /*child:  Image(
-          // image: NetworkImage("https://images.pexels.com/photos/1535162/pexels-photo-1535162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", scale: 1))
-          //image: AssetImage("assets/pexels-photo-1535162.jpeg"))
-        )*/
-        // child: Image.asset("assets/pexels-photo-1535162.jpeg"),
-        // child: Image.network("https://images.pexels.com/photos/1535162/pexels-photo-1535162.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
-        
-
-        // Buttons
-        // https://docs.flutter.dev/release/breaking-changes/buttons
-        /*child: TextButton(
-          onPressed: null,
-          child: const Text("hello"),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>((Colors.red[100])!),
-            ),
-          ),
-          */
-          /*hild: ElevatedButton(
-          onPressed: null,
-          child: const Text("hello"),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>((Colors.red[100])!),
-            ),
-          ),*/
-
-
-        /*
-          child: ElevatedButton.icon(
-            onPressed: null, 
-            icon: const Icon(
-              Icons.download,
-              color: Colors.black,
-
-            ), 
-            label: const Text(
-              "hello",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-
-            ),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>((Colors.yellow[800])!)
-            ),
-            ),
-            */
-            
-            child: IconButton(
-              onPressed: null,
-              icon: Icon(
-                Icons.mail,
-                size: 50,
-                color: Colors.blue,
-                ),
-              ),
-          
-      ),
+      body:  Container(),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         child: Icon(
